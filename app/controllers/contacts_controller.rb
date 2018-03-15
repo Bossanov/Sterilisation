d2bcc5a444aa1423784a086e62eb6d69eff80ea5
Antoine STAUMONT <staumont.antoine@me.com>
2018-03-15 17:56:51 +0100

    finished bug

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,10 +9,10 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash[:notice] = 'Merci pour votre message. Nous vous recontacterons très vite!'
-      redirect_to :root
+      redirect_to root_path
     else
       flash[:alert] = 'Un problème est survenu. Veuillez réessayer svp.'
-      redirect_to :root
+      redirect_to root_path
     end
   end
 
